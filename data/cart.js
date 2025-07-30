f870b0a4a8be4b48ab34c387d909e1f937f3bd5f
nandhini0609 <nandhiniprabu0608@gmail.com>
2025-07-30 15:14:14 +0530

    saved cart to the local Storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,20 @@
-export let cart = [{
-    productsId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 1
-}, {
-    productsId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 2
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'))
+
+if (!cart) {
+    cart = [{
+        productsId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 1
+    }, {
+        productsId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 2
+    }];
+};
+
+
+
+function saveToCart() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productsId) {
     let matchingItem;
@@ -22,6 +32,7 @@ export function addToCart(productsId) {
         });
 
     }
+    saveToCart();
 };
 
 export function removingCartItem(productsId) {
@@ -34,5 +45,6 @@ export function removingCartItem(productsId) {
 
     });
     cart = newCart;
+    saveToCart();
 
 }
