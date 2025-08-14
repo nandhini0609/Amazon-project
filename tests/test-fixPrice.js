824eeb85e8efa66ffa451b052407504435e722f8
nandhini0609 <nandhiniprabu0608@gmail.com>
2025-08-14 20:38:47 +0530

    practiced Automated testing with basic test and edge test

---
+++ tests/test-fixPrice.js
@@ -0,0 +1,23 @@
+import { formattingPrice } from "../scripts/utils.js/fixPrice.js";
+console.log('test suite:Automated testing for the formatting price)')
+
+console.log('testing with random number')
+if (formattingPrice(2095) === '20.95') {
+    console.log('passes')
+} else {
+    console.log('fails')
+}
+
+console.log('testing with Zero')
+if (formattingPrice(0) === '0.00') {
+    console.log('passes')
+} else {
+    console.log('fails')
+}
+
+console.log('testing with negative number')
+if (formattingPrice(2000.5) === '20.01') {
+    console.log('passes')
+} else {
+    console.log('fails')
+}
