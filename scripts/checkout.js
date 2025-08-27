b479ec68c3861b61ca3e229d74c3fb5ccf312c36
nandhini0609 <nandhiniprabu0608@gmail.com>
2025-08-27 17:25:02 +0530

    changes our promises to the async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,24 +1,3 @@
-// On page load, set cart quantity from localStorage in checkout-header-middle-section
-document.addEventListener('DOMContentLoaded', () => {
-    const cartQuantityElement = document.querySelector('.return-to-home-link');
-    let cartQuantity = 0;
-    const cartData = localStorage.getItem('cart');
-    if (cartData) {
-        try {
-            const cartObj = JSON.parse(cartData);
-            if (Array.isArray(cartObj)) {
-                cartQuantity = cartObj.reduce((sum, item) => sum + (item.quantity || 0), 0);
-            } else if (typeof cartObj === 'object' && cartObj !== null) {
-                cartQuantity = Object.values(cartObj).reduce((sum, qty) => sum + (typeof qty === 'number' ? qty : 0), 0);
-            }
-        } catch (e) {
-            cartQuantity = 0;
-        }
-    }
-    if (cartQuantityElement) {
-        cartQuantityElement.textContent = cartQuantity + ` ` + `items`;
-    }
-});
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import '../data/cart-class.js';
@@ -26,20 +5,57 @@ import { loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import '../data/cart-backend.js';
 
-
-
-Promise.all([
-    loadProductsFetch()
-    ,
-    new Promise((resolve) => {
+async function loadPage() {
+    await loadProductsFetch();
+    await new Promise((resolve) => {
         loadCart(() => {
             resolve();
         })
-
     })
-]).then((values) => {
-    console.log(values)
     renderOrderSummary();
     renderPaymentSummary();
-});
+}
+loadPage();
+
+// Promise.all([
+//     loadProductsFetch()
+//     ,
+//     new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         })
+
+//     })
+// ]).then((values) => {
+//     console.log(values)
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// })
+
+// new Promise((resolve) => {
+
+//     loadProducts(() => {
+//         resolve('value1');
+//     });
+// }).then((value) => {
+//     console.log(value)
+//     return new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         })
+
+//     })
+
+// }).then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// })
+
+// loadProducts(() => {
+//     loadCart(() => {
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     })
+
+// });
 
